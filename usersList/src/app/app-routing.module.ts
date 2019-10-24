a80092f25e22f77dc2fc8c0a0c94c98949a9ef27
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-10-24 15:46:08 +0300

    update

--- usersList/src/app/app-routing.module.ts
+++ usersList/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:ListComponent
+  },
+
+{
+    path:'add',component:AddUserComponent 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- usersList/src/app/app.module.ts
+++ usersList/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { AddUserComponent } from './components/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
