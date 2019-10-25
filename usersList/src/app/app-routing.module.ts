079ffd809f5afdf20d907459e28f128f828d5d28
Esra Karakaş <33701879+Esrakaraks@users.noreply.github.com>
2019-10-25 20:23:44 +0300

    update

--- usersList/src/app/app-routing.module.ts
+++ usersList/src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   },
 
 {
-    path:'userdata',component:SettingsUserComponent 
+    path:'userdata/:id',component:SettingsUserComponent 
   }
 ];
 
--- usersList/src/app/components/settings-user/settings-user.component.ts
+++ usersList/src/app/components/settings-user/settings-user.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
+import { ListComponent } from './../list/list.component';
+import { Component, OnInit,Input } from '@angular/core';
+import {Post} from '../list/post';
 @Component({
   selector: 'app-settings-user',
   templateUrl: './settings-user.component.html',
   styleUrls: ['./settings-user.component.css']
 })
 export class SettingsUserComponent implements OnInit {
-
+  post: Post[] = [];
+  
   constructor() { }
 
   ngOnInit() {
   }
+
+      
+      
+ 
 
 }
